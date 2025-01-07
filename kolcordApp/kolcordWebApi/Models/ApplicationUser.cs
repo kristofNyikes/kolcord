36c37565d7c87d962c2ef2076e20f823c97990fa
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-07 11:36:22 +0100

    feat: create initial models

---
+++ kolcordApp/kolcordWebApi/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace kolcordWebApi.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string Avatar { get; set; } =
+        "https://res.cloudinary.com/dilctmmuh/image/upload/v1736240885/riixinvcxidpygnqdexg.png";
+    [StringLength(500, ErrorMessage = "Bio length cannot be more then 500 characters")]
+    public string Bio { get; set; } = string.Empty;
+    public ICollection<ServerMember> Servers { get; set; } = new List<ServerMember>();
+    public ICollection<Friendship> Friendships { get; set; } = new List<Friendship>();
+    public ICollection<Message> Messages { get; set; } = new List<Message>();
+}
---
+++ kolcordApp/kolcordWebApi/Models/Server.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kolcordWebApi.Models;
+
+public class Server
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Server name is required")]
+    [MinLength(3, ErrorMessage = "Server name cannot be shorter than 3 characters")]
+    [StringLength(40, ErrorMessage = "Server name cannot be longer than 40 characters")]
+    public string Name { get; set; }
+    [StringLength(1000, ErrorMessage = "Server description cannot be longer than 1000 characters")]
+    public string Description { get; set; } = string.Empty;
+    public string OwnerId { get; set; }
+    public ApplicationUser Owner { get; set; }
+    public string IconUrl { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
