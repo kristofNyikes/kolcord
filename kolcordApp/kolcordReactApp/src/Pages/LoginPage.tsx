6a564f5270a53e475934e3aa98fe1924a925a6aa
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-08 18:30:06 +0100

    feat: implement register and login pages

--- kolcordApp/kolcordReactApp/src/Pages/LoginPage.tsx
+++ kolcordApp/kolcordReactApp/src/Pages/LoginPage.tsx
@@ -1,5 +1,24 @@
+import { useState } from 'react';
+import InputField from '../Components/InputField/InputField';
+import SubmitButton from '../Components/Buttons/SubmitButton';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <form className="bg-black bg-opacity-30 rounded-lg shadow-lg p-5 flex flex-col items-center  mt-24 md:mt-16">
+      <p className='text-xl mb-5 md:text-2xl'>Welcome back we missed you!</p>
+        <InputField inputValue={email} inputState={setEmail} type="email">
+          Email:{' '}
+        </InputField>
+        <InputField inputValue={password} inputState={setPassword} type="password">
+          Pasword:{' '}
+        </InputField>
+        <SubmitButton>Submit</SubmitButton>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- kolcordApp/kolcordReactApp/src/Pages/RegisterPage.tsx
+++ kolcordApp/kolcordReactApp/src/Pages/RegisterPage.tsx
@@ -1,5 +1,31 @@
-const RegisterPage = () => {
-  return <div>RegisterPage</div>;
-};
+import { useState } from 'react';
+import InputField from '../Components/InputField/InputField';
+import SubmitButton from '../Components/Buttons/SubmitButton';
+import { Link } from 'react-router-dom';
 
+const RegisterPage = () => {
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  return (
+    <div className="flex justify-center items-center">
+      <form className="bg-black bg-opacity-30 rounded-lg shadow-lg p-5 flex flex-col items-center  mt-24 md:mt-16">
+        <p className="text-xl mb-5 md:text-2xl">Create an account</p>
+        <InputField inputValue={username} inputState={setUsername} type="text">
+          Username:{' '}
+        </InputField>
+        <InputField inputValue={email} inputState={setEmail} type="email">
+          Email:{' '}
+        </InputField>
+        <InputField inputValue={password} inputState={setPassword} type="password">
+          Pasword:{' '}
+        </InputField>
+        <SubmitButton>Submit</SubmitButton>
+        <Link to={'/login'} className="hover:text-red-700">
+          Already have an account?
+        </Link>
+      </form>
+    </div>
+  );
+};
 export default RegisterPage;
