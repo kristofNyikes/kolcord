5fd9808c7fadef30dc5e2f5fa810e07aa6089afc
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-08 15:19:56 +0100

    feat: change background color

--- kolcordApp/kolcordReactApp/src/App.tsx
+++ kolcordApp/kolcordReactApp/src/App.tsx
@@ -3,7 +3,7 @@ import Logo from './Components/Logo/Logo';
 
 function App() {
   return (
-    <div className='bg-gradient-to-b from-red-950 to-stone-950 min-h-screen text-white'>
+    <div className='bg-gradient-to-t from-red-950 via-stone-950 to-red-950 min-h-screen text-white'>
       <Logo/>
       <Outlet />
     </div>
