39c2915573516fddc0af474ca54c1a468264f0ef
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-08 17:34:52 +0100

    fix: add route prop so the route is not hardcoded

--- kolcordApp/kolcordReactApp/src/Components/Buttons/SignUpInButton.tsx
+++ kolcordApp/kolcordReactApp/src/Components/Buttons/SignUpInButton.tsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom'
 
 type SignUpInButton  ={
   children: React.ReactNode;
+  route: string;
 }
 
-const SignUpInButton = ({ children } : SignUpInButton) => {
+const SignUpInButton = ({ children, route } : SignUpInButton) => {
   return (
-    <Link to="/register" className="bg-red-800 rounded text-xl px-4 py-2 mx-4 border-4 border-transparent hover:border-black hover:text-black">
+    <Link to={`/${route}`} className="bg-red-800 rounded text-xl px-4 py-2 mx-4 border-4 border-transparent hover:border-black hover:text-black">
           {children}
         </Link>
   )
