37350d8bbf81c582755d96fe04e15a9dc19a55cc
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-09 15:49:50 +0100

    refactor: add refresh token in NewUserDto

--- kolcordApp/kolcordWebApi/Dtos/Account/NewUserDto.cs
+++ kolcordApp/kolcordWebApi/Dtos/Account/NewUserDto.cs
@@ -5,4 +5,5 @@ public class NewUserDto
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Token { get; set; }
+    public string RefreshToken { get; set; }
 }
