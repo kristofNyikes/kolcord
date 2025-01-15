b570ec056658bd185ae6243ae97fc2d4a29103fb
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-15 12:53:32 +0100

    fix: add route to friendship controller

--- kolcordApp/kolcordWebApi/Controllers/FriendshipController.cs
+++ kolcordApp/kolcordWebApi/Controllers/FriendshipController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace kolcordWebApi.Controllers;
 
+[Route("api/friendship")]
+[ApiController]
 public class FriendshipController : ControllerBase
 {
     private readonly IFriendshipRepository _friendshipRepo;
