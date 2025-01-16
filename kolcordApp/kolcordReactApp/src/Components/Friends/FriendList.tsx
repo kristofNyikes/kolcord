2442e15fc6d6fd08d3ba78fafede1ce6c7709664
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-01-16 19:03:26 +0100

    fix: firnedlist display logic fixed

--- kolcordApp/kolcordReactApp/src/Components/Friends/FriendList.tsx
+++ kolcordApp/kolcordReactApp/src/Components/Friends/FriendList.tsx
@@ -1,47 +1,42 @@
-import { useEffect, useState } from 'react'
-import FriendItem from './FriendItem'
-import { fetchWithTokenCheck } from '../../Helpers/fetchWithTokenRefresh'
+import { useEffect, useState } from 'react';
+import FriendItem from './FriendItem';
+import { fetchWithTokenCheck } from '../../Helpers/fetchWithTokenRefresh';
 
 type Friend = {
   id: number;
   userId: string;
   friendDto: FriendDto;
-  
-}
+};
 type FriendDto = {
   id: string;
   avatar: string;
   bio: string;
   userName: string;
-}
+};
 
 const FriendList = () => {
   const [friendList, setFriendList] = useState<Friend[] | null>(null);
 
   useEffect(() => {
     const fetchFriends = async () => {
-      const response = await fetchWithTokenCheck("/api/friendship/friend-list", {});
-      if(response.ok) {
+      const response = await fetchWithTokenCheck('/api/friendship/friend-list', {});
+      if (response.ok) {
         const data = await response.json();
-        console.log(data)
-        console.log(data[0])
-        console.log(data[0].friendDto)
         setFriendList(data);
       }
-    }
+    };
     fetchFriends();
-  }, [])
+  }, []);
 
   return (
     <div>
-      {friendList ? friendList.map(f => {
-        return <FriendItem friend={f.friendDto} key={f.id}/>
-      }) : "placeholder"
-    
-      }
+      {friendList && friendList.length > 0
+        ? friendList.map((f) => {
+            return <FriendItem friend={f.friendDto} key={f.id} />;
+          })
+        : 'no friends lol'}
     </div>
-    // <div></div>
-  )
-}
+  );
+};
 
-export default FriendList
+export default FriendList;
