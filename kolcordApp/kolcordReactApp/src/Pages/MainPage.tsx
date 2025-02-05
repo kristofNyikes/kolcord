e227a53a639af87caa5fda7d5311051f0058f857
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-02-05 20:20:42 +0100

    feat: fetch friend requests on mount

--- kolcordApp/kolcordReactApp/src/Pages/MainPage.tsx
+++ kolcordApp/kolcordReactApp/src/Pages/MainPage.tsx
@@ -1,7 +1,38 @@
-import FriendList from '../Components/Friends/FriendList';
+import ServerList from '../Components/ServerList/ServerList';
+import TopBar from '../Components/TopBar/TopBar';
+import { Outlet } from 'react-router';
+import { fetchWithTokenCheck } from '../Helpers/fetchWithTokenRefresh';
+import { useEffect, useState } from 'react';
+import { Requests } from '../types/types';
+
 
 const MainPage = () => {
-  return (<FriendList/>)
+  const [friendRequests, setFriendRequests] = useState<Requests[] | null>(null);
+
+  useEffect(() => {
+      const fetchFriends = async () => {
+        const response = await fetchWithTokenCheck('/api/friendship/friend-requests', {});
+        if (response.ok) {
+          const data = await response.json();
+          setFriendRequests(data);
+        }
+      };
+      fetchFriends();
+    }, []);
+
+  return (
+    <div>
+      <TopBar/>
+      <div className="flex flex-row overflow-hidden">
+        <div className="w-16 flex-none overflow-y-auto">
+          <ServerList />
+        </div>
+        <div className="flex-grow bg-black/35 p-2 h-screen w-screen">
+          <Outlet context={friendRequests}/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
