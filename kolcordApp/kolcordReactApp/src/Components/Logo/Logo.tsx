55470519d2c2b81ccb9d487c0f32ece2897bb443
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-02-05 20:17:25 +0100

    refactor: remove padding

--- kolcordApp/kolcordReactApp/src/Components/Logo/Logo.tsx
+++ kolcordApp/kolcordReactApp/src/Components/Logo/Logo.tsx
@@ -7,7 +7,7 @@ type Logo = {
 const Logo  = ({route} : Logo) => {
 
   return (
-    <Link to={route} className='flex justify-center md:justify-start md:p-5'>
+    <Link to={route} className='flex justify-center pt-3 md:justify-start md:p-5'>
           <img src="main-icon.png" alt="main icon" className="w-20" />
           <h1 className="text-7xl font-oswald content-center px-4 text-black">Kolcord</h1>
         </Link>
