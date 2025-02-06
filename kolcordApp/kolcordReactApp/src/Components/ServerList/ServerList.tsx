60e562055cd4be192fae0741c4819e22c0add367
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-02-06 17:19:50 +0100

    feat: add placeholder spinner on server list

--- kolcordApp/kolcordReactApp/src/Components/ServerList/ServerList.tsx
+++ kolcordApp/kolcordReactApp/src/Components/ServerList/ServerList.tsx
@@ -1,9 +1,12 @@
-
+import Spinner from '../Spinner/Spinner';
 
 const ServerList = () => {
   return (
-    <div className='bg-black/60 w-full h-full'>¯\_(ツ)_/¯</div>
-  )
-}
+    <div className="bg-black/60 w-full h-full flex flex-col items-center gap-4">
+      ¯\_(ツ)_/¯
+      <Spinner size={32}/>
+    </div>
+  );
+};
 
-export default ServerList
+export default ServerList;
