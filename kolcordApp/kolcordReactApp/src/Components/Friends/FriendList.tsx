ea3361dc31fd200018bfb1ee4e4e9b6907aa839d
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-03-05 18:29:12 +0100

    feat: realtime friend list update on new friendship creation

--- kolcordApp/kolcordReactApp/src/Components/Friends/FriendList.tsx
+++ kolcordApp/kolcordReactApp/src/Components/Friends/FriendList.tsx
@@ -2,22 +2,32 @@ import { useEffect, useState } from 'react';
 import { fetchWithTokenCheck } from '../../Helpers/fetchWithTokenRefresh';
 import Spinner from '../Spinner/Spinner';
 import FriendItem from './FriendItem';
-
-type Friend = {
-  id: number;
-  userId: string;
-  friendDto: FriendDto;
-};
-type FriendDto = {
-  id: string;
-  avatar: string;
-  bio: string;
-  userName: string;
-};
+import { Friend } from '../../types/types';
+import * as signalR from '@microsoft/signalr';
 
 const FriendList = () => {
   const [friendList, setFriendList] = useState<Friend[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const apiUrl = import.meta.env.VITE_BASE_URL;
+
+  useEffect(() => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl(`${apiUrl}/friendRequestsHub`, {
+        accessTokenFactory: () => localStorage.getItem('accessToken') || '',
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    connection.start().then(() => {
+      connection.on('NewFriendship', (newFriendShip: Friend) => {
+        setFriendList((prevFriendList) => prevFriendList ? [...prevFriendList, newFriendShip] : [newFriendShip])
+      });
+    }).catch(err => console.error('SignalR Connection Error: ', err));
+
+    return () => {
+      connection.stop();
+    }
+  }, [apiUrl]);
 
   useEffect(() => {
     const fetchFriends = async () => {
@@ -33,12 +43,16 @@ const FriendList = () => {
   }, []);
 
   return (
-    <div className='flex flex-col items-center'>
-      {friendList && friendList.length > 0
-        ? friendList.map((f) => {
-            return <FriendItem friend={f.friendDto} key={f.id} />;
-          })
-        : isLoading ? <Spinner/> : <p>No friends</p>}
+    <div className="flex flex-col items-center">
+      {friendList && friendList.length > 0 ? (
+        friendList.map((f) => {
+          return <FriendItem friend={f.friendDto} key={f.id} />;
+        })
+      ) : isLoading ? (
+        <Spinner />
+      ) : (
+        <p>No friends</p>
+      )}
     </div>
   );
 };
