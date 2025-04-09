7ebc9676579f641a4a3517d02976392ed187eaeb
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-04-09 16:49:52 +0200

    feat: create interface fore message repository

---
+++ kolcordApp/kolcordWebApi/Interfaces/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using kolcordWebApi.Models;
+
+namespace kolcordWebApi.Interfaces;
+
+public interface IMessageRepository
+{
+    public Task<Conversation> GetOrCreateDirectConversation(string userId1, string userId2);
+    public Task<GroupConversation> CreateGroupConversation(string creatorId, string name, List<string> memberIds);
+    public Task<Message> SendMessage(string senderId, int conversationId, string content);
+    public Task<List<Message>> GetMessages(int conversationId, int skip = 0, int take = 10);
+    public Task<List<Conversation>> GetUserConversation(string userId);
+}
