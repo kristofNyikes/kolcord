5585de7e5f6c6d358c314644f57cdcb3e7312be7
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-04-10 13:45:01 +0200

    style: remove commented code

--- kolcordApp/kolcordWebApi/Hubs/FriendRequestsHub.cs
+++ kolcordApp/kolcordWebApi/Hubs/FriendRequestsHub.cs
@@ -9,10 +9,7 @@ public class FriendRequestsHub : Hub
 {
     public override async Task OnConnectedAsync()
     {
-        //Console.WriteLine($"Connection established: {Context.ConnectionId}");
-        //Console.WriteLine($"signalR hub on connect user id: {Context.UserIdentifier}");
         await base.OnConnectedAsync();
-        //await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
     }
     public async Task SendFriendRequest(string userId, string friendId)
     {
