fc69e1df6551f6e1c14f4191804338e24aab185b
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-11-02 18:12:52 +0100

    feat: setup testing environment

---
+++ kolcordApp/kolcordWebApi.Tests/Helpers/TestTester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolcordWebApi.Tests.Helpers
+{
+    public class TestTester
+    {
+        [Fact]
+        public void TestSetup_ShouldWork()
+        {
+            var expected = 4;
+            var actual = 2 + 2;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
