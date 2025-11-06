5718ec867ceec9c59b1b5792cc28d24c4f64aebc
Kristóf Nyikes <kristof.nyikes@gmail.com>
2025-11-06 19:44:05 +0100

    style: formatting

--- kolcordApp/kolcordReactApp/src/Routes/Routes.tsx
+++ kolcordApp/kolcordReactApp/src/Routes/Routes.tsx
@@ -1,35 +1,38 @@
-import { createBrowserRouter } from 'react-router';
-import App from '../App';
-import HomePage from '../Pages/HomePage';
-import LoginPage from '../Pages/LoginPage';
-import RegisterPage from '../Pages/RegisterPage';
-import MainPage from '../Pages/MainPage';
-import ProtectedRoute from './ProtectedRoute';
-import FriendList from '../Components/Friends/FriendList';
-import FriendRequests from '../Components/Friends/FriendRequests';
-import Search from '../Components/Search/Search';
-import ConversationPage from '../Pages/ConversationPage';
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import HomePage from "../Pages/HomePage";
+import LoginPage from "../Pages/LoginPage";
+import RegisterPage from "../Pages/RegisterPage";
+import MainPage from "../Pages/MainPage";
+import ProtectedRoute from "./ProtectedRoute";
+import FriendList from "../Components/Friends/FriendList";
+import FriendRequests from "../Components/Friends/FriendRequests";
+import Search from "../Components/Search/Search";
+import ConversationPage from "../Pages/ConversationPage";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
-      { path: '', element: <HomePage /> },
-      { path: 'login', element: <LoginPage /> },
-      { path: 'register', element: <RegisterPage /> },
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "register", element: <RegisterPage /> },
       {
-        path: 'main',
+        path: "main",
         element: (
           <ProtectedRoute>
             <MainPage />
           </ProtectedRoute>
         ),
         children: [
-          { path: '', element: <FriendList /> },
-          { path: 'friend-requests', element: <FriendRequests /> },
-          { path: 'search', element: <Search /> },
-          { path: 'conversations/:conversationId', element: <ConversationPage /> },
+          { path: "", element: <FriendList /> },
+          { path: "friend-requests", element: <FriendRequests /> },
+          { path: "search", element: <Search /> },
+          {
+            path: "conversations/:conversationId",
+            element: <ConversationPage />,
+          },
         ],
       },
     ],
